3ddab70a84485daf7b697ad776d632757f5d2e75
saul-melo <melo.saul123@icloud.com>
2021-01-02 14:27:51 -0600

    Years remaining in your life

--- New Year 2021.playground/Contents.swift
+++ New Year 2021.playground/Contents.swift
@@ -1,5 +1,14 @@
 import UIKit
+import GameKit
 
 var newYearMessage = "Happy New Year 2021"
 
+private func randomNumberGenerator(range: Int) -> Int {
+    let random = GKRandomSource.sharedRandom().nextInt(upperBound: range)
+    return random
+}
+
 print(newYearMessage)
+print("Years remaining in your life: \(randomNumberGenerator(range: 100))")
+
+
