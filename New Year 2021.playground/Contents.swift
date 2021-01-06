85c2a620c63799c340d919124b87e56cc343a21a
saul-melo <melo.saul123@icloud.com>
2021-01-06 14:58:20 -0600

    Lifetime tragedies

--- New Year 2021.playground/Contents.swift
+++ New Year 2021.playground/Contents.swift
@@ -9,7 +9,9 @@ private func randomNumberGenerator(range: Int) -> Int {
 }
 
 print(newYearMessage)
+print()
 print("Remaining years to live: \(randomNumberGenerator(range: 101))")
 print("Publically traded companies founded: \(randomNumberGenerator(range: 3))")
 print("Lifetime trips to Mars: \(randomNumberGenerator(range: 4))")
 print("U.S. states never visited (excluding travel stops): \(randomNumberGenerator(range: 31))")
+print("Lifetime tragedies: \(randomNumberGenerator(range: 4))")
