5332ce99644ae94c840b1a3cb625b07827ef3a5b
saul-melo <melo.saul123@icloud.com>
2021-01-30 14:44:11 -0600

    Furniture items assembled

--- New Year 2021.playground/Contents.swift
+++ New Year 2021.playground/Contents.swift
@@ -41,4 +41,5 @@ print("Patents held: \(randomNumberGenerator(range: 6))")
 print("Marathons run: \(randomNumberGenerator(range: 4))")
 print("Words coined: \(randomNumberGenerator(range: 2))")
 print("Earthquakes experienced: \(randomNumberGenerator(range: 4))")
+print("Furniture items assembled: \(randomNumberGenerator2(min: 10, max: 40))")
 
