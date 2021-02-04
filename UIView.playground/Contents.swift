44552e21a6b78ba6816f81f5d42fde24ddbf3028
saul-melo <melo.saul123@icloud.com>
2021-02-03 18:01:55 -0600

    racecar

--- UIView.playground/Contents.swift
+++ UIView.playground/Contents.swift
@@ -9,17 +9,23 @@ class MyViewController : UIViewController {
         view.backgroundColor = .white
 
         let label_1 = UILabel()
-        label_1.frame = CGRect(x: 120, y: 310, width: 200, height: 20)
-        label_1.text = "remotely working"
+        label_1.frame = CGRect(x: 120, y: 20, width: 200, height: 20)
+        label_1.text = "never odd or even"
         label_1.textColor = .black
         
         let label_2 = UILabel()
-        label_2.frame = CGRect(x: 120, y: 330, width: 200, height: 20)
-        label_2.text = "infinitely content"
+        label_2.frame = CGRect(x: 120, y: 40, width: 200, height: 20)
+        label_2.text = "a toyota is a toyota"
         label_2.textColor = .black
+        
+        let label_3 = UILabel()
+        label_3.frame = CGRect(x: 120, y: 60, width: 200, height: 20)
+        label_3.text = "racecar"
+        label_3.textColor = .black
         
         view.addSubview(label_1)
         view.addSubview(label_2)
+        view.addSubview(label_3)
         self.view = view
     }
 }
